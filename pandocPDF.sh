03b518d0e427e68ab78238c1a2beffd88296b5f5
scotartt <scot.mcphee@gmail.com>
2014-01-29 22:30:07 +1000

    changed location of bibliofile to automatically generated one from mendeley

--- pandocPDF.sh
+++ pandocPDF.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 outfile=${1%.*}
 dropbox=/Users/smcphee/Dropbox
-pandoc -f markdown -t latex -V geometry:margin=4cm -V fontsize=12pt -V papersize=a4paper -V documentclass=memoir -V classoption=oneside -V classoption=article -V mainfont='Times New Roman' -V frenchspacing -V onehalfspacing --smart --standalone --latex-engine=xelatex --bibliography=${dropbox}/ANCH/PHD/THESIS/99-Bibliography/phd.bib --csl=${dropbox}/Libraries/pandoc/classics-standard-for-uq-rhd-theses.csl --output=${outfile}.PDF ${1}
+pandoc -f markdown -t latex -V geometry:margin=4cm -V fontsize=12pt -V papersize=a4paper -V documentclass=memoir -V classoption=oneside -V classoption=article -V mainfont='Times New Roman' -V frenchspacing -V onehalfspacing --smart --standalone --latex-engine=xelatex --bibliography=${dropbox}/ANCH/PHD/THESIS/99-Bibliography/library.bib --csl=${dropbox}/Libraries/pandoc/classics-standard-for-uq-rhd-theses.csl --output=${outfile}.PDF ${1}
