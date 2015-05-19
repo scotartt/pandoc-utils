219940ae76c2b3a7687dc7440978ce739a93991a
scotartt <scot.mcphee@gmail.com>
2015-05-19 18:10:29 +1000

    bulk changes - paths and tweaks. better template.

---
+++ pandoc10ptPDF.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+outfile=${1%.*}
+dropbox=/Users/smcphee/Dropbox
+/usr/local/bin/pandoc -f markdown -t latex -V geometry:margin=3cm -V fontsize=10pt -V papersize=a4paper -V documentclass=memoir -V classoption=oneside -V classoption=article -V mainfont='Times New Roman' --smart --standalone --latex-engine=xelatex --bibliography=${dropbox}/ANCH/PHD/THESIS/99-Bibliography/library_clean.bib --csl=${dropbox}/Libraries/pandoc/classics-standard-for-uq-rhd-theses.csl --output=${outfile}.PDF ${1}
--- pandocWORD.sh
+++ pandocWORD.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 outfile=${1%.*}
 dropbox=/Users/smcphee/Dropbox
-pandoc -f markdown -t docx --smart --standalone --bibliography=${dropbox}/ANCH/PHD/THESIS/99-Bibliography/library.bib --csl=${dropbox}/Libraries/pandoc/classics-standard-for-uq-rhd-theses.csl --output=${outfile}.docx ${1}
+pandoc -f markdown -t docx --smart --standalone --bibliography=${dropbox}/ANCH/PHD/THESIS/99-Bibliography/library_clean.bib --csl=${dropbox}/Libraries/pandoc/classics-standard-for-uq-rhd-theses.csl --output=${outfile}.docx ${1}
 
