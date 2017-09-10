a999fa9917f94351be8cf2cbf322a195e67ac01c
scotartt <scot.mcphee@gmail.com>
2017-09-10 19:49:02 +1000

    PDF/LaTeX templating tweaked

--- pandocWORD.sh
+++ pandocWORD.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
-outfile=${1%.*}
+filename=$(basename $1)
+outfile=${filename%.*}
+currentdir=$(pwd)
+echo "PDF DESTINATION IS $currentdir/$outfile.PDF"
 dropbox=/Users/smcphee/Dropbox
 pandoc -f markdown -t docx --smart --standalone --bibliography=${dropbox}/ANCH/PHD/THESIS/99-Bibliography/library_clean.bib --csl=${dropbox}/Libraries/pandoc/classics-standard-for-uq-rhd-theses.csl --output=${outfile}.docx ${1}
 
