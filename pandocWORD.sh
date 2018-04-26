07bf418352918b68a40faa6b58654a032fd082c9
Scot Mcphee <scot.mcphee@gmail.com>
2018-04-26 13:37:39 +1000

    modified to work with pandoc v2

--- pandocWORD.sh
+++ pandocWORD.sh
@@ -4,5 +4,5 @@ outfile=${filename%.*}
 currentdir=$(pwd)
 echo "PDF DESTINATION IS $currentdir/$outfile.PDF"
 dropbox=/Users/smcphee/Dropbox
-pandoc -f markdown -t docx --smart --standalone --bibliography=${dropbox}/ANCH/PHD/THESIS/99-Bibliography/library_clean.bib --csl=${dropbox}/Libraries/pandoc/classics-standard-for-uq-rhd-theses.csl --output=${outfile}.docx ${1}
+pandoc -f markdown -t docx+smart --standalone --bibliography=${dropbox}/ANCH/PHD/THESIS/99-Bibliography/library_clean.bib --csl=${dropbox}/Libraries/pandoc/classics-standard-for-uq-rhd-theses.csl --output=${outfile}.docx ${1}
 
