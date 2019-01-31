67a66636731f1ba0c31dbad5e0a0921e3f02f419
Scot Mcphee <scot.mcphee@virginaustralia.com>
2019-01-31 13:31:46 +1000

    refining thesis production

--- pandoc_brief_PDF.sh
+++ pandoc_brief_PDF.sh
@@ -4,4 +4,4 @@ outfile=${filename%.*}
 currentdir=$(pwd)
 echo "PDF DESTINATION IS $currentdir/$outfile.PDF"
 dropbox=/Users/smcphee/Dropbox
-/usr/local/bin/pandoc -f markdown+smart -t latex -V geometry:margin=4cm -V fontsize=12pt -V papersize=a4paper -V documentclass=memoir -V classoption=oneside -V classoption=article -V mainfont='Times New Roman' -V frenchspacing -V onehalfspacing -V biblio-title='Works Cited' -V noparindent=true --standalone --pdf-engine=xelatex --bibliography=${dropbox}/ANCH/PHD/THESIS/99-Bibliography/library_clean.bib --csl=${dropbox}/Libraries/pandoc/classics-standard-for-uq-rhd-theses.csl --output=${outfile}.PDF ${1} 
+/usr/local/bin/pandoc -f markdown+smart -t latex -V geometry:margin=4cm -V fontsize=10pt -V papersize=a4paper -V documentclass=memoir -V classoption=oneside -V classoption=article -V mainfont='Times New Roman' -V frenchspacing -V onehalfspacing -V biblio-title='Works Cited' -V noparindent=true --standalone --pdf-engine=xelatex --bibliography=${dropbox}/ANCH/PHD/THESIS/99-Bibliography/library_clean.bib --csl=${dropbox}/Libraries/pandoc/classics-standard-for-uq-rhd-theses.csl --output=${outfile}.PDF ${1} 
